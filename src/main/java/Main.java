4fe241164a3b1731d8c68610a7eb84575bc65a53
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2016-02-22 08:52:49 -0500

    First

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+
+public class Main {
+
+    public String readRawDataToString() throws Exception{
+        Path path = Paths.get(ClassLoader.getSystemResource("RawData.txt").toURI());
+        return new String(readAllBytes(get(path.toUri())));
+    }
+
+    public static void main(String[] args) throws Exception{
+
+    }
+}
